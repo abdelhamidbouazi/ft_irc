5119d9e527072ae87e3b6a62803ef62bd74017a8
Abdelbari Messah <amessah@e3r1p11.1337.ma>
2023-10-18 00:48:30 +0100

    adding the two branches

--- srcs/ConnectingSocket.cpp
+++ srcs/ConnectingSocket.cpp
@@ -5,3 +5,9 @@ HDE::ConnectingSocket::ConnectingSocket(int domain, int service, int protocol, i
    set_connection(connect_to_network(get_sock(), get_address()));
    test_connection(get_connection());
 }
+
+
+int HDE::ConnectingSocket::connect_to_network(int sock, struct sockaddr_in address)
+{
+    return connect(sock, (struct sockaddr *)&address, sizeof(address)); 
+}
