fb75742271fcddf336cc53828377d69f40913f2c
Abdelbari Messah <amessah@e3r2p13.1337.ma>
2023-10-19 20:51:01 +0100

    push the server last touch

---
+++ includes/TestServer.hpp
@@ -0,0 +1,23 @@
+#ifndef TESTSERVER_HPP
+#define TESTSERVER_HPP
+
+#include "SimpleServer.hpp"
+
+namespace HDE
+{
+    class TestServer : public SimpleServer
+    {
+        private:
+            char buffer[30000];
+            int new_socket;
+            void accepter();
+            void handler();
+            void responder();
+        public:
+            TestServer();
+            void launch();
+    };
+}
+
+
+#endif // TESTSERVER_HPP
---
+++ srcs/SimpleServer.cpp
@@ -0,0 +1,12 @@
+#include "../includes/SimpleServer.hpp"
+
+
+HDE::SimpleServer::SimpleServer(int domain, int service, int protocol, int port, u_long interface, int bklg)
+{
+    this->socket = new ListeningSocket(domain, service, protocol, port, interface, bklg);
+}
+
+HDE::ListeningSocket *HDE::SimpleServer::get_socket()
+{
+    return this->socket;
+}
