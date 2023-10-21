5c1fc3297329f667a14a500ad960f22a3aa1e196
Abdelbari Messah <amessah@e3r2p13.1337.ma>
2023-10-21 18:25:02 +0100

    push the start of using poll and fcntl

--- includes/socket.hpp
+++ includes/socket.hpp
@@ -1,9 +1,42 @@
 #ifndef SOCKET_HPP
 #define SOCKET_HPP
 
-#include <sys/socket.h>
+// #include <sys/socket.h>
+// #include <iostream>
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/ioctl.h>
+// #include <sys/poll.h>
+// #include <sys/time.h>
+// #include <netinet/in.h>
+// #include <errno.h>
+
+
+
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <unistd.h>
+#include <fstream>
+#include <stdio.h>
 #include <iostream>
+#include <sys/time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <cstdlib>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/poll.h>
+#include <sys/ioctl.h>
+#include <map>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <sstream>
+#include <netdb.h>
+#include <fcntl.h>
+#include <chrono>
+#include <vector>
 
 namespace HDE 
 {
@@ -17,6 +50,7 @@ namespace HDE
 			SocketHde(int domain, int service, int protocol, int port, unsigned long interface);
 			virtual int connect_network( int sock, struct sockaddr_in address) = 0;
 			void test_connection(int item_to_test);
+			void test_connection_for_setsockopt(int item_to_test);
 			struct sockaddr_in get_address();
 			int get_sock();
 			int get_connection();
