501e93109e936e9c34bee339e4e76af5b85f914c
Abdelhamid Bouazi <abouazi@e2r6p11.1337.ma>
2023-10-22 15:10:44 +0100

    Update: Fixed errors in Parsing PORT and PASSWORD

--- srcs/Args.cpp
+++ srcs/Args.cpp
@@ -4,24 +4,19 @@ Args::Args(std::string port, std::string password)
 {
 	if (port.length() > 6 || port.length() < 2)
 		throw std::length_error("Port length error");
-	std::istringstream iss(port);
-	int portValue;
-	if (iss >> portValue)
-	{
-		if (portValue < 0 || portValue > 65535)
-			throw std::out_of_range("Invalid Port");
-
-		this->Port = portValue;
-
-		if (password.length() < 8)
-			throw std::length_error("Password length error");
-		else
-			this->Password = password;
+	for (int i = 0; i < port.size(); i++){
+		if (!isdigit(port[i]))
+			throw std::length_error("Bad Port!");
 	}
+	double portValue = std::atof(port.c_str());
+	if (portValue < 0 || portValue > 65535)
+		throw std::out_of_range("Invalid Port");
+	this->Port = portValue;
+	std::cout << this->Port;
+	if (password.length() < 8)
+		throw std::length_error("Password length error");
 	else
-	{
-		throw std::length_error("Bad Port!");
-	}
+		this->Password = password;
 }
 
 Args::~Args()
