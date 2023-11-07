365beeedd916134c3b5078cdcd8994b71da992c9
Abdelbari Messah <amessah@e2r7p10.1337.ma>
2023-11-07 11:13:55 +0100

    commit in NewBranch to pull

--- srcs/Commands/Join.cpp
+++ srcs/Commands/Join.cpp
@@ -4,5 +4,20 @@
 
 void Join(std::vector<std::pair<std::string , std::string > > joinVector, Client &c ,std::map<std::string, Channel&> channelsMap)
 {
-    
+    if(joinVector.empty())
+    {
+        // replies ERR_NEEDMOREPARAMS
+        return ;
+    }
+    if(joinVector.size() == 1 && joinVector[0].first == "0")
+    {
+        //leave all the channel 
+        // decrement the count of channel to 0 in the client 
+        return ;
+    }
+
+    for(int i = 0; i <= joinVector.size(); i++)
+    {
+        
+    }
 }
