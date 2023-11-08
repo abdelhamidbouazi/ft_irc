9015a17fe6c35fdad72a42f167d085f5e3363c4f
Abdelhamid Bouazi <abouazi@e2r7p13.1337.ma>
2023-11-08 15:07:00 +0100

    Update: mode

--- srcs/Commands/Mode.cpp
+++ srcs/Commands/Mode.cpp
@@ -112,7 +112,7 @@ bool modeO(std::vector<std::string> message, std::map<std::string, Channel &> &c
 
 bool modeL(std::vector<std::string> message, std::map<std::string, Channel &> &channelsMap, int mode, Client &c)
 {
-
+	std::cout << "Entred to the limit users\n";
 	if (channelsMap.find(message[1]) != channelsMap.end())
 	{
 		// check if the message[4] is an int, if its not int return false
@@ -131,6 +131,9 @@ bool modeL(std::vector<std::string> message, std::map<std::string, Channel &> &c
 
 bool HDE::SocketHde::CheckMODE(std::vector<std::string> message, int i)
 {
+	int mode;
+	Channel ch("#channel", clt.at(fds[i].fd), "");
+	channelsMap.insert(std::pair<std::string, Channel &>("#channel", ch));
 	if (channelsMap.empty())
 	{
 		std::cout << "Empty list of channels" << std::endl;
@@ -141,86 +144,62 @@ bool HDE::SocketHde::CheckMODE(std::vector<std::string> message, int i)
 		std::cout << "args error" << std::endl;
 		return false;
 	}
-	Channel ch("#channel", clt.at(fds[i].fd), "");
-	channelsMap.insert(std::pair<std::string, Channel &>("#channel", ch));
+
+	if (message[2].compare("+") == 0)
+	{
+		mode = 1;
+	}
+	else if (message[2].compare("-") == 0)
+	{
+		mode = 0;
+	}
+	else
+	{
+		return false;
+	}
+	std::cout << "mode number setted to : " << mode << std::endl;
+	for (int j = 0; j < message.size(); j++) {
+		std::cout << "**" << message[j] << "**" << std::endl;
+	}
 	if (message[0].compare("MODE") == 0)
 	{
-		if (message[2].compare("+") == 0)
+		if (message[3].compare("i") == 0)
 		{
-			if (message[3].compare("i") == 0)
-			{
-				if (modeI(message, channelsMap, 1))
-					return true;
-				return false;
-				// clt.at(fds[i].fd).getClientId
-			}
-			else if (message[3].compare("t") == 0)
-			{
-				if (modeT(message, channelsMap, 1))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("k") == 0)
-			{
-				if (modeK(message, channelsMap, 1))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("o") == 0)
-			{
-				if (modeO(message, channelsMap, 1, clt.at(fds[i].fd)))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("l") == 0)
-			{
-				if (modeL(message, channelsMap, 1, clt.at(fds[i].fd)))
-					return true;
-				return false;
-			}
-			else
-			{
-				std::cout << "invalid mode" << std::endl;
-			}
+			if (modeI(message, channelsMap, mode))
+				return true;
+			return false;
+			// clt.at(fds[i].fd).getClientId
 		}
-		if (message[2].compare("-") == 0)
+		else if (message[3].compare("t") == 0)
 		{
-			if (message[3].compare("i") == 0)
-			{
-				if (modeI(message, channelsMap, 0))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("t") == 0)
-			{
-				if (modeT(message, channelsMap, 0))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("k") == 0)
-			{
-				if (modeK(message, channelsMap, 0))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("o") == 0)
-			{
-				if (modeO(message, channelsMap, 0, clt.at(fds[i].fd)))
-					return true;
-				return false;
-			}
-			else if (message[3].compare("l") == 0)
-			{
-				if (modeL(message, channelsMap, 0, clt.at(fds[i].fd)))
-					return true;
-				return false;
-			}
-			else
-			{
-				std::cout << "invalid mode" << std::endl;
-			}
+			if (modeT(message, channelsMap, mode))
+				return true;
+			return false;
 		}
-
+		else if (message[3].compare("k") == 0)
+		{
+			if (modeK(message, channelsMap, mode))
+				return true;
+			return false;
+		}
+		else if (message[3].compare("o") == 0)
+		{
+			if (modeO(message, channelsMap, mode, clt.at(fds[i].fd)))
+				return true;
+			return false;
+		}
+		else if (message[3].compare("l ") == 0)
+		{
+			if (modeL(message, channelsMap, mode, clt.at(fds[i].fd))) {
+				std::cout << "MODE limit users set to : " << channelsMap.at(message[1]).getLimitUsers() << " in channel : " << channelsMap.at(message[1]).getChannelName() << std::endl;
+				return true;
+			}
+			return false;
+		}
+		else
+		{
+			std::cout << "invalid mode" << std::endl;
+		}
 	}
 	std::cout << "MODE" << std::endl;
 	return false;
